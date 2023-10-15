9dedebdaa90e5f85df2a3420d6a59c87b359c1ad
Girish Venkatachalam <girish@gayatri-hitech.com>
2023-10-15 09:36:09 +0530

    Fix css for centering

--- packages/vue/src/Blackgallery.vue
+++ packages/vue/src/Blackgallery.vue
@@ -37,6 +37,28 @@ imageList:  [{
 
 <template>
 
+   <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+
+   <script>
+      const options = {
+            bottom: '64px', // default: '32px'
+            right: 'unset', // default: '32px'
+            left: '32px', // default: 'unset'
+            time: '0.5s', // default: '0.3s'
+            mixColor: '#fff', // default: '#fff'
+            backgroundColor: '#fff',  // default: '#fff'
+            buttonColorDark: '#100f2c',  // default: '#100f2c'
+            buttonColorLight: '#fff', // default: '#fff'
+            saveInCookies: false, // default: true,
+            label: '🌓', // default: ''
+            autoMatchOsTheme: true // default: true
+      }
+
+      const darkmode = new Darkmode(options);
+      darkmode.showWidget();
+   </script>
+
+ 
  <h1> Blackgallery </h1>
   <!-- XXX This is the gallery switcher -->
     <ul class="gallery-nav"> 
